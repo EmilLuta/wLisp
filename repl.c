c876a9d73d63464e330efb90f5674c2f3c9447dd
EmilLuta <virgil.luta@gmail.com>
2018-12-05 00:03:11 +0200

    Add support for parsing and AST generation

--- repl.c
+++ repl.c
@@ -31,16 +31,37 @@ void add_history(char* unused);
 #endif
 
 int main(int argc, char** argv) {
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expression = mpc_new("expression");
+    mpc_parser_t* wLisp = mpc_new("wLisp");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "                                                               \
+            number : /-?[0-9]+/ ;                                       \
+            operator : '+' | '-' | '*' | '/' ;                          \
+            expression : <number> | '(' <operator> <expression>+ ')';   \
+            wLisp : /^/ <operator> <expression>+ /$/ ;                  \
+        ",
+        Number, Operator, Expression, wLisp);
     puts("wLisp v0.0.0.1");
 
     while (1) {
         char* input = readline("> ");
         add_history(input);
-
-        printf("Nope, %s\n", input);
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, wLisp, &r)) {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+        /* printf("Nope, %s\n", input); */
 
         free(input);
     }
 
+    mpc_cleanup(4, Number, Operator, Expression, wLisp);
     return 0;
 }
